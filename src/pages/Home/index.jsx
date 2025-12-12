5d5559a28ac03e013465492fecf1b16146818a88
GwonWooL <229861@naver.com>
2025-12-12 11:40:46 +0900

    feat: 로그인 기능 구현

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,20 +5,41 @@ import "./index.css"
 
 export default function Home() {
   const [login, setLogin] = useState(false)
+  const [id, setId] = useState("")
+  const [pw, setPw] = useState("")
+
+  const Login = () => {
+    if (id === "test123" && pw === "test123password") {
+      alert("로그인 성공했습니다.")
+      setLogin(false)
+    } else {
+      alert("로그인 실패했습니다.")
+    }
+  }
 
   return (
     <div className="home">
       <h3>Welcome to Main Page!</h3>
       <button onClick={() => setLogin(true)}>로그인</button>
+
       {login &&
         createPortal(
           <PortalModalContainer>
             <div className="loginModalInner">
-              <input placeholder="test123" />
-              <input type="password" placeholder="test123password" />
+              <input
+                placeholder="test123"
+                value={id}
+                onChange={(e) => setId(e.target.value)}
+              />
+              <input
+                type="password"
+                placeholder="test123password"
+                value={pw}
+                onChange={(e) => setPw(e.target.value)}
+              />
 
               <div className="btns">
-                <button>로그인</button>
+                <button onClick={Login}>로그인</button>
                 <button onClick={() => setLogin(false)}>취소</button>
               </div>
             </div>
