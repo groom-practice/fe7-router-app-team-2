92a1e9e5179d7ff9202d8141488c454bbe80dbc7
GwonWooLee <143773589+GwonWooL@users.noreply.github.com>
2025-12-12 11:25:13 +0900

    feat: 홈페이지 및 로그인 모달 구현 (#8)
    Co-authored-by: Lee Yujin <84452202+JIN921@users.noreply.github.com>

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,30 @@
+import { useState } from "react"
+import { createPortal } from "react-dom"
+import PortalModalContainer from "../../components/PortalModalContainer"
+import "./index.css"
+
 export default function Home() {
-  return <div>Home Page</div>;
+  const [login, setLogin] = useState(false)
+
+  return (
+    <div className="home">
+      <h3>Welcome to Main Page!</h3>
+      <button onClick={() => setLogin(true)}>로그인</button>
+      {login &&
+        createPortal(
+          <PortalModalContainer>
+            <div className="loginModalInner">
+              <input placeholder="test123" />
+              <input type="password" placeholder="test123password" />
+
+              <div className="btns">
+                <button>로그인</button>
+                <button onClick={() => setLogin(false)}>취소</button>
+              </div>
+            </div>
+          </PortalModalContainer>,
+          document.body
+        )}
+    </div>
+  )
 }
